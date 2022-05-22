92174d9587be366b31c55d56238ae4237a0cfcbf
data-pata <39658922+data-pata@users.noreply.github.com>
2022-05-22 14:38:01 +0200

    1.1 course information, step1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+const Header = (props) => {
+  return <h1> {props.course} </h1>;
+};
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.numExercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -6,21 +22,16 @@ const App = () => {
   const exercises2 = 7;
   const part3 = "State of a component";
   const exercises3 = 14;
+  const total = exercises1 + exercises2 + exercises3;
 
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+    <>
+      <Header course={course} />
+      <Content part={part1} numExercises={exercises1} />
+      <Content part={part2} numExercises={exercises2} />
+      <Content part={part3} numExercises={exercises3} />
+      <Total total={total} />
+    </>
   );
 };
 
